094bb18f2df93eaadeb568792cd3828aa3140199
Akshay Gohrava <akshaygohrava@gmail.com>
2026-02-09 09:41:17 +0530

    intial commit

--- src/Components/React-Routing/Effect2.jsx
+++ src/Components/React-Routing/Effect2.jsx
@@ -3,18 +3,22 @@ import Routes from './Routes'
 import { useEffect, useState } from 'react'
 
 export default function Effect2() {
-  const [quotes, setQuotes] = useState(null);
+  const [quotes, setQuotes] = useState([]);
 
   useEffect(() => {
     fetch("https://dummyjson.com/quotes")
     .then(response => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+       console.log(data);
+       setQuotes(data.quotes || []);
+    }) 
     .catch((error) => console.log(error))
   }, [])
   return (
     <>
          <h2>This is Effect2 Component !</h2>
          <p>Use Effect Hooks in API Fetching</p>
+         <div>{quotes.map((q, index) => <p key={index}>{q.quote}</p>)}</div>
          <Routes />
     </>
   )
