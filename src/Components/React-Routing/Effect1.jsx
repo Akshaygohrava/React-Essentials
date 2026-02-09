f9dbb1d5e411c2f0c7d6ade47981df3edc713dd0
Akshay Gohrava <akshaygohrava@gmail.com>
2026-02-09 09:43:51 +0530

    initial commit

--- src/Components/React-Routing/Effect1.jsx
+++ src/Components/React-Routing/Effect1.jsx
@@ -24,13 +24,14 @@ const Effect1 = () => {
 
     return (
         <>
+          <Routes />
         <h2>Intro To Use Effect Hooks !</h2>
            <h2>This is Effect1 Component !</h2>
            <p>Use effect is used when , the variable or component data changes, then the useeffect hooks will call the function !</p>
            <button onClick={() => setClick(click + 1)}>Use Effect Hooks</button>
            <h2>Here is Counter : {click}</h2>
            <p id="useeffect">Changes render here , when Button click , Using UseEffect !</p>
-           <Routes />
+           
 
         </>
     )
--- src/Components/React-Routing/Effect2.jsx
+++ src/Components/React-Routing/Effect2.jsx
@@ -17,9 +17,9 @@ export default function Effect2() {
   return (
     <>
          <h2>This is Effect2 Component !</h2>
+         <Routes />
          <p>Use Effect Hooks in API Fetching</p>
          <div>{quotes.map((q, index) => <p key={index}>{q.quote}</p>)}</div>
-         <Routes />
     </>
   )
 }
