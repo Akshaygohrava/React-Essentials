5a90d3ac85416aa20094f8397594dea3c5f55be6
Akshay Gohrava <akshaygohrava@gmail.com>
2026-03-11 21:47:45 +0530

    initial commit

--- src/Components/React-Routing/Router.jsx
+++ src/Components/React-Routing/Router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 import Home from './Home'
 import Effect1 from './Effect1'
 import Effect2 from './Effect2'
@@ -9,46 +9,33 @@ import Formhandling2 from './Formhandling2'
 import Formhandling3 from './Formhandling3'
 import Formhandling4 from './Formhandling4'
 import Context from '../Data/Context'
-const router = createBrowserRouter ([
-    {
-        path: "/",
-        element: <Home />
-    },{
-        path: "use-effect-1",
-        element: <Effect1 />
-    },{
-        path: "use-effect-2",
-        element: <Effect2 />
-    },{
-        path: "Products",
-        element: <Products />
-    },{
-        path: "Recipes",
-        element: <Recipes />
-    },{
-        path: "Formhandling1",
-        element: <Formhandling1 />
-    },{
-        path: "Formhandling2",
-        element: <Formhandling2 />
-    },{
-        path: "Formhandling3",
-        element: <Formhandling3 />
-    },{
-        path: "Formhandling4",
-        element: <Formhandling4 />
-    },{
-        path: "Context",
-        element: <Context />
-    }
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <h1>React Router</h1>
+        <Outlet />
+      </>
+    ),
+    children: [
+      { index: true, element: <Home /> },
+      { path: "use-effect-1", element: <Effect1 /> },
+      { path: "use-effect-2", element: <Effect2 /> },
+      { path: "Products", element: <Products /> },
+      { path: "Recipes", element: <Recipes /> },
+      { path: "Formhandling1", element: <Formhandling1 /> },
+      { path: "Formhandling2", element: <Formhandling2 /> },
+      { path: "Formhandling3", element: <Formhandling3 /> },
+      { path: "Formhandling4", element: <Formhandling4 /> },
+      { path: "Context", element: <Context /> }
+    ]
+  }
 ])
+
 const Router = () => {
-    return (
-        <>
-          <h1>React Router</h1>
-          <RouterProvider router={router} />
-          
-        </>
-    )
+    return <RouterProvider router={router} />;
 }
+
 export default Router
